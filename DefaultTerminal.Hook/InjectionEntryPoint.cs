64695d3da59fdd179827c1f15e25fc299fe92c48
Kevin <iodesme@gmail.com>
2020-06-06 06:40:39 +0900

    Fix OnCreateProcessHook explorer error

--- DefaultTerminal.Hook/InjectionEntryPoint.cs
+++ DefaultTerminal.Hook/InjectionEntryPoint.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading;
 using DefaultTerminal.Hook.Natives;
@@ -24,25 +26,49 @@ namespace DefaultTerminal.Hook
         private bool OnCreateProcessHook(string lpApplicationName, string lpCommandLine, ref SECURITY_ATTRIBUTES lpProcessAttributes, ref SECURITY_ATTRIBUTES lpThreadAttributes, bool bInheritHandles, uint dwCreationFlags, IntPtr lpEnvironment, string lpCurrentDirectory, [In] ref STARTUPINFO lpStartupInfo, out PROCESS_INFORMATION lpProcessInformation)
         {
             bool isRedirected = false;
-            var applicationName = lpApplicationName;
-            var commandLine = lpCommandLine;
 
-            // When dwCreationFlags is not CREATE_NO_WINDOW and lpApplicationName exists
-            // Attempts to redirect the hooked request to Windows Terminal.
-            if ((dwCreationFlags & 0x08000000) == 0 && !string.IsNullOrEmpty(lpApplicationName))
+            // Only when dwCreationFlags is not CREATE_NO_WINDOW
+            if ((dwCreationFlags & 0x08000000) == 0)
             {
-                applicationName = null;
-                commandLine = $"wt {lpCommandLine.Replace(";", "\\;")}";
-                isRedirected = true;
+                var currentProcess = Process.GetProcessById(RemoteHooking.GetCurrentProcessId());
+
+                if (currentProcess.ProcessName == "explorer")
+                {
+                    // If Windows Explorer, redirect only in some cases
+                    switch (Path.GetFileName(lpApplicationName))
+                    {
+                        case "powershell.exe":
+                        case "cmd.exe":
+                        case null:
+                            isRedirected = Redirect(ref lpApplicationName, ref lpCommandLine);
+                            break;
+                    }
+                }
+                else if (!string.IsNullOrEmpty(lpApplicationName))
+                {
+                    // In other cases, redirect only when lpApplicationName exists
+                    isRedirected = Redirect(ref lpApplicationName, ref lpCommandLine);
+                }
             }
 
-            var result = NativeMethods.CreateProcess(applicationName, commandLine, ref lpProcessAttributes,
-                ref lpThreadAttributes, bInheritHandles, dwCreationFlags, lpEnvironment, lpCurrentDirectory,
-                ref lpStartupInfo, out lpProcessInformation);
+            var result = NativeMethods.CreateProcess(lpApplicationName, lpCommandLine, ref lpProcessAttributes, ref lpThreadAttributes,
+                bInheritHandles, dwCreationFlags, lpEnvironment, lpCurrentDirectory, ref lpStartupInfo, out lpProcessInformation);
 
             Server.RaiseProcessCreated(lpApplicationName, lpCommandLine, lpProcessInformation.dwProcessId, isRedirected);
 
             return result;
+        }
+        #endregion
+
+        #region Private Methods
+        private bool Redirect(ref string lpApplicationName, ref string lpCommandLine)
+        {
+            if (!string.IsNullOrEmpty(lpApplicationName))
+                lpApplicationName = null;
+
+            lpCommandLine = $"wt {lpCommandLine.Replace(";", "\\;")}";
+
+            return true;
         }
         #endregion
 
